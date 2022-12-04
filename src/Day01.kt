bb5df7ba6271f6dd0281f0e5d3d98b016546120c
Vi Quang <vquang@gmail.com>
2022-12-03 20:55:49 -0800

    Add Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,57 @@
+import java.util.PriorityQueue
+
+private data class Elf(val calorieList: MutableList<Int> = mutableListOf()) {
+    fun totalCalories() : Int {
+        return calorieList.sumOf { it }
+    }
+}
+
 fun main() {
+
+    fun createElfList(input : List<String>) : MutableList<Elf> {
+        val elves = mutableListOf<Elf>()
+        var currentElf = Elf()
+
+        for (line in input) {
+            if (line.isEmpty()) {
+                elves.add(currentElf)
+                currentElf = Elf()
+            } else {
+                currentElf.calorieList.add(line.toInt())
+            }
+        }
+        return elves
+    }
+
+    fun findElvesWithTheMostCalories(elves: List<Elf>, numberOfElves:Int) : PriorityQueue<Elf> {
+        val priorityQueue = PriorityQueue<Elf> {
+                e1, e2 -> e1.totalCalories() - e2.totalCalories()
+        }
+
+        for (elf in elves) {
+            priorityQueue.add(elf)
+            if (priorityQueue.size > numberOfElves) {
+                priorityQueue.poll()
+            }
+        }
+
+        return priorityQueue
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = createElfList(input)
+        val priorityQueue = findElvesWithTheMostCalories(elves, 1)
+        return priorityQueue.sumOf { it.totalCalories() }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = createElfList(input)
+        val priorityQueue = findElvesWithTheMostCalories(elves, 3)
+        return priorityQueue.sumOf { it.totalCalories() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput(1)
     println(part1(input))
     println(part2(input))
 }
